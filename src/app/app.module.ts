164760f77c05bdbe213deab1ad1252f7f54834da
Kherin <kherin.j.s.bundhoo@gmail.com>
2020-10-21 21:07:03 +0400

    restructured folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CustomerComponent } from './customer/customer.component';
 
 import { ReactiveFormsModule } from '@angular/forms';
-import { CardDetailComponent } from './card-detail/card-detail.component';
-
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomerComponent,
-    CardDetailComponent
   ],
   imports: [
     BrowserModule,
